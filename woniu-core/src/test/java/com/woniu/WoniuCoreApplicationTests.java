93a98a820369997f1fb5c9401dfcaa81a2c2d51b
pangshw <pangshw@hotmail.com>
2020-11-11 17:42:52 +0800

    dev

--- woniu-core/src/test/java/com/woniu/WoniuCoreApplicationTests.java
+++ woniu-core/src/test/java/com/woniu/WoniuCoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.woniu;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WoniuCoreApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.woniu;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WoniuCoreApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
