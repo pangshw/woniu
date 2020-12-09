8a22286d28e5c317e54b5db4697ca8bdfece124a
pangshw <pangshw@hotmail.com>
2020-12-09 20:21:32 +0800

    dev

---
+++ woniu-ui/src/utils/common.ts
@@ -0,0 +1,24 @@
+export function tableScrollHeight() {
+  let dom = document.getElementsByClassName("page-content")[0];
+  if (!dom) {
+    return 0;
+  }
+  let h = dom.clientHeight
+    - parseInt(getStyle(dom, 'marginTop')) - parseInt(getStyle(dom, 'marginBottom'))
+    - document.getElementsByClassName("ant-table-thead")[0].clientHeight;
+
+  dom.childNodes.forEach((item: any, index) => {
+    if (!item.getAttribute('class') || item.getAttribute('class').indexOf('ant-table-wrapper') === -1) {
+      h -= item.clientHeight;
+    }
+  });
+  return h;
+}
+
+const getStyle = (obj: any, attr: any) => {
+  if (obj.currentStyle) {
+    return obj.currentStyle[attr];
+  } else {
+    return document.defaultView.getComputedStyle(obj, null)[attr];
+  }
+}
